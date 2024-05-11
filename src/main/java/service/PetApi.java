e73f50fe5eb2edc1c2bc8b91c2dbb0fe132da8b4
lubchynsky <lubchynsky.v@outlook.com>
2024-05-11 15:48:46 +0200

    Update PetsApi

--- src/main/java/service/PetApi.java
+++ src/main/java/service/PetApi.java
@@ -2,6 +2,7 @@ package service;
 
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
+import io.restassured.response.ValidatableResponse;
 import io.restassured.specification.RequestSpecification;
 import model.PetModel;
 
@@ -32,5 +33,47 @@ public class PetApi {
                 .body()
                 .jsonPath()
                 .getList(".", PetModel.class);
+    }
+
+    public ValidatableResponse getPetRawResponse(long id) {
+        return given()
+                .spec(getRequestSpec())
+                .when()
+                .pathParam("id", id)
+                .get()
+                .then();
+    }
+
+    public PetModel getPet(long id) {
+        return getPetRawResponse(id)
+                .statusCode(200)
+                .extract()
+                .as(PetModel.class);
+    }
+
+    public ValidatableResponse postPetRawResponse(PetModel petModel) {
+        return given()
+                .spec(getRequestSpec())
+                .when()
+                .body(petModel)
+                .post()
+                .then();
+    }
+
+    public PetModel postPet(PetModel petModel) {
+        return postPetRawResponse(petModel)
+                .statusCode(201)
+                .extract()
+                .as(PetModel.class);
+    }
+
+    public void deletePet(long id) {
+        given()
+                .spec(getRequestSpec())
+                .when()
+                .pathParam("id", id)
+                .delete()
+                .then()
+                .statusCode(200);
     }
 }
